77521107a162052b687449bc4f74f1b0e267a6f7
Alexey Abramov <alexpoligon@mail.ru>
2019-08-18 17:34:49 +0300

    Revert "remove all files for review"
    This reverts commit 0f887fb24c5961de6133e3adb189ee8ac7c1af78.

---
+++ lesson_2/sources/ip_filter.cpp
@@ -0,0 +1,38 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "ip_filter.h"
+
+int main(int, char const**)
+{
+    try
+    {
+        ip_filter::ip_pool ips;      
+        for(std::string line; std::getline(std::cin, line);)
+        {
+            const auto ips_as_strings = ip_filter::split_to_strings(line, '\t');
+            const auto result = ip_filter::split_to_tokens(ips_as_strings.at(0), '.');
+            ips.push_back( result );
+        }
+        // reverse lexicographically sort
+        ip_filter::reverse_lexicographic_sort(ips);
+        ip_filter::print( ips );
+
+        // filter by first byte and output
+        ip_filter::print( ip_filter::filter(ips, 1) );
+
+        // filter by first and second bytes and output
+        ip_filter::print( ip_filter::filter(ips, 46, 70) );
+
+        // filter by any byte and output
+        ip_filter::print( ip_filter::filter_any(ips, 46) );
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
